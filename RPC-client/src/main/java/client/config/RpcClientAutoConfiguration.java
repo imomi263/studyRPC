5c538bdb667f9c3adfa7719fcdc9f5ee60ba954f
imomi263 <imomi263@foxmail.com>
2024-12-05 23:15:39 +0800

    rpc-client

--- RPC-client/src/main/java/client/config/RpcClientAutoConfiguration.java
+++ RPC-client/src/main/java/client/config/RpcClientAutoConfiguration.java
@@ -1,6 +1,6 @@
 package client.config;
 
-import client.processor.RpcClientProcessor;
+
 import client.proxy.ClientStubProxyFactory;
 import core.balance.FullRoundBalance;
 import core.balance.LoadBalance;
@@ -32,37 +32,10 @@ public class RpcClientAutoConfiguration {
     @ConditionalOnMissingBean
     public ClientStubProxyFactory clientStubProxyFactory(){
         return new ClientStubProxyFactory();
+
     }
 
-    @Primary //@Primary 更高优先级
-    @Bean(name = "loadBalance")
-    @ConditionalOnMissingBean
-    @ConditionalOnProperty(prefix = "rpc.client", name = "balance", havingValue = "randomBalance", matchIfMissing = true)
-    public LoadBalance randomBalance() {
-        return new RandomBalance();
-    }
 
-    @Bean(name = "loadBalance")
-    @ConditionalOnMissingBean
-    @ConditionalOnProperty(prefix = "rpc.client", name = "balance", havingValue = "fullRoundBalance")
-    public LoadBalance loadBalance() {
-        return new FullRoundBalance();
-    }
 
-    @Bean
-    @ConditionalOnMissingBean
-    @ConditionalOnBean({RpcClientProperties.class, LoadBalance.class})
-    public DiscoveryService discoveryService(@Autowired RpcClientProperties properties,@Autowired LoadBalance loadBalance) {
-//        return new ZookeeperDiscoveryService(properties.getDiscoveryAddr(), loadBalance);
-        return new RedisDiscoveryService(8099,"110.41.139.215","p@ssw0rd",1,loadBalance);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public RpcClientProcessor rpcClientProcessor(@Autowired ClientStubProxyFactory clientStubProxyFactory,
-                                                 @Autowired DiscoveryService discoveryService,
-                                                 @Autowired RpcClientProperties properties) {
-        return new RpcClientProcessor(clientStubProxyFactory, discoveryService, properties);
-    }
 
 }
